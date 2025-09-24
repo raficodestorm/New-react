a21cba0f4184a8d7f8bccf1cf86360d62cea1c12
S-A-Rafi <sarafi3258@gmail.com>
2025-09-24 13:06:22 +0600

    update

--- react-tamplate-pr/src/components/Sidebar.jsx
+++ react-tamplate-pr/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ function Sidebar() {
 <aside className="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
   {/* Brand Logo */}
   <a href="../../index3.html" className="brand-link">
-    <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
+    <img src="../../assets-admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
     <span className="brand-text font-weight-light">AdminLTE 3</span>
   </a>
   {/* Sidebar */}
@@ -14,7 +14,7 @@ function Sidebar() {
     {/* Sidebar user (optional) */}
     <div className="user-panel mt-3 pb-3 mb-3 d-flex">
       <div className="image">
-        <img src="../../dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+        <img src="../../assets-admin/dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
       </div>
       <div className="info">
         <a href="#" className="d-block">Alexander Pierce</a>
