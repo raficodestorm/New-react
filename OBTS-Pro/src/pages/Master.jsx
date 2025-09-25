4789a96a6424b1b0f84b677f088e413adb72f67e
s-a-rafi <sarafi3258@gmail.com>
2025-09-25 08:13:32 +0600

    up

--- OBTS-Pro/src/pages/Master.jsx
+++ OBTS-Pro/src/pages/Master.jsx
@@ -1,8 +1,12 @@
-
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
 function Master() {
     return (
         <>
-
+            <Navbar></Navbar>
+            <Sidebar></Sidebar>
+            <Footer></Footer>
         </>
     )
 }
